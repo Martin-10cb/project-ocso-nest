219a89f8b4825435b41983bb066690b573c3085b
Martin-10cb <martincalzadillab@outlook.com>
2024-09-27 12:08:36 -0600

    Upload photo in employee resource

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -12,5 +12,10 @@ export class Employee {
     phoneNumber: string;
     @Column('text')
     email: string;
+    @Column ({
+        type: 'text',
+        nullable: true
+    })
+    photoUrl: string;
 }
 
