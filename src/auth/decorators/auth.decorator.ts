4308e920fa44695e4af5012595f7c8e8338d7024
Martin-10cb <martincalzadillab@outlook.com>
2024-09-30 11:50:40 -0600

    Auth for routes

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,11 +1,12 @@
 import { applyDecorators, UseGuards } from "@nestjs/common"
-import { Roles } from "./roles.decorator"
 import { AuthGuard } from "../guards/auth.guards"
 import { RolesGuard } from "../guards/roles.guards"
+import { ROLES } from "../constants/roles.constants"
+import { Roles } from "./roles.decorator"
 
 
-export const Auth = (...roles:string[]) => {
-    roles.push("Admin")
+export const Auth = (...roles:ROLES[]) => {
+    roles.push(ROLES.ADMIN)
     return applyDecorators(
         Roles(roles),
         UseGuards(AuthGuard, RolesGuard)
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -5,29 +5,33 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { UserData } from 'src/auth/decorators/user.decorators';
 import { User } from 'src/auth/entities/user.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
-  @Auth("Employee")
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee")) throw new UnauthorizedException ("No estas autorizado, solo admins");
     return this.providersService.findAll();
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/name/:name')
   findByName(@Param('name')name:string){
     return this.providersService.findOneByName(name)
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     const provider = this.providersService.findOne(id);
@@ -35,11 +39,13 @@ export class ProvidersController {
     return provider;
   }
 
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providersService.update(id, updateProviderDto);
   }
 
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providersService.remove(id);
