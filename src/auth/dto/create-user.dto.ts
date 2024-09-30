88576d031b034866bf3e311c76adc25ab82a8381
Martin-10cb <martincalzadillab@outlook.com>
2024-09-30 11:15:23 -0600

    Update user route and better names for employee Entity

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { IsEmail, IsIn, IsOptional, IsString, MinLength } from "class-validator";
 import { User } from "../entities/user.entity";
 
 export class CreateUserDto extends User{
@@ -8,5 +8,8 @@ export class CreateUserDto extends User{
     @IsString()
     @MinLength(8)
     userPassword: string;
+    @IsOptional()
+    @IsIn(["Admin", "Emloyee", "Manager"])
+    useRoles: string[];
 
 }
