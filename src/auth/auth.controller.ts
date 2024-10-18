4ac6503e34c98422a6fbc3ce87de8eafa5b353a3
Martin-10cb <martincalzadillab@outlook.com>
2024-10-18 10:44:32 -0600

    Implementacion de las cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -6,6 +6,10 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiAuth } from './decorators/api.decorator';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
+import { Response } from 'express';
+import { TOKEN_NAME } from './constants/jwt.constants';
+import { Cookies } from './decorators/cookies.decorator';
+
 
 
 @ApiAuth()
@@ -34,8 +38,15 @@ export class AuthController {
       userPassword: "71bsi1972",
     } as User
   })
-  login(@Body() loginUserDto :LoginUserDto){
-    return this.authService.loginUser(loginUserDto)
+  async login(@Body() loginUserDto :LoginUserDto, @Res({passthrough: true}) response:Response, @Cookies() cookies: any){
+    const token = await this.authService.loginUser(loginUserDto)
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000*60*60*24*7,
+    })
+    return 
   }
 
   @Patch("/:email")
