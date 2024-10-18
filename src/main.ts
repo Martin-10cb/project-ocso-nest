1675296c066312c038c9347b258d8288921018bd
Martin-10cb <martincalzadillab@outlook.com>
2024-10-18 08:41:01 -0600

    feat: Enable cors for front

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    cors: {
+      origin: process.env.allowedOrigin
+    }
+  });
   const config = new DocumentBuilder()
     .setTitle('Ocso API')
     .setDescription('API for ocso management')
@@ -20,6 +24,6 @@ async function bootstrap() {
       transform: true
     }
   ))
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
